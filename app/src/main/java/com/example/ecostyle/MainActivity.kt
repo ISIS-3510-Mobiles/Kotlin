b2deb3ac40d9604cd48363d60fba3cca4d663176
Felipe34515 <f.rueda4@uniandes.edu.co>
2024-09-15 14:13:22 -0500

    Inicio sesión con FirebaseUI Auth

--- app/src/main/java/com/example/ecostyle/MainActivity.kt
+++ app/src/main/java/com/example/ecostyle/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.ecostyle
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            val response = IdpResponse.fromResultIntent(it.data)
+
+            if (it.resultCode == RESULT_OK){
+                val user = FirebaseAuth.getInstance().currentUser
+                if (user != null){
+                    Toast.makeText( this, "Bienvenido", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        }.launch(AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build())
     }
 }
