870916dbc3deb6d5c6f2e3b5d23a421c4c392889
Felipe34515 <f.rueda4@uniandes.edu.co>
2024-09-15 14:59:22 -0500

    Retener sesióna activa

--- app/src/main/java/com/example/ecostyle/MainActivity.kt
+++ app/src/main/java/com/example/ecostyle/MainActivity.kt
@@ -12,31 +12,52 @@ import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+        configAuth()
+    }
 
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            val response = IdpResponse.fromResultIntent(it.data)
+    private fun configAuth(){
+        firebaseAuth = FirebaseAuth.getInstance()
+        authStateListener = FirebaseAuth.AuthStateListener { auth ->
+            if (auth.currentUser != null) {
+                supportActionBar?.title = auth.currentUser?.displayName
+            } else {
+                val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
 
-            if (it.resultCode == RESULT_OK){
-                val user = FirebaseAuth.getInstance().currentUser
-                if (user != null){
-                    Toast.makeText( this, "Bienvenido", Toast.LENGTH_SHORT).show()
-                }
+                registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+                    val response = IdpResponse.fromResultIntent(it.data)
+
+                    if (it.resultCode == RESULT_OK){
+                        val user = FirebaseAuth.getInstance().currentUser
+                        if (user != null){
+                            Toast.makeText( this, "Bienvenido", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }.launch(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build())
             }
 
-        }.launch(AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build())
+        }
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        firebaseAuth.addAuthStateListener(authStateListener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        firebaseAuth.removeAuthStateListener(authStateListener)
     }
 }
